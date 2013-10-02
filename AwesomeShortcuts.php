e1e80bdba45f7597ad6c2e1ac144bfed386fa482
Fabian Becker <fabian.becker@uni-tuebingen.de>
2013-10-02 23:47:00 +0200

    Remove getInformation function to make it a proper plugin

--- AwesomeShortcuts.php
+++ AwesomeShortcuts.php
@@ -4,26 +4,13 @@ namespace Piwik\Plugins\AwesomeShortcuts;
 class AwesomeShortcuts extends \Piwik\Plugin {
 
     /**
-     * @see Piwik\Plugin::getInformation
-     */
-    public function getInformation()
-    {
-        return array(
-            'description'     => Piwik_Translate("AwesomeShortcuts_PluginDescription"),
-            'author'          => 'Fabian Becker',
-            'author_homepage' => 'http://geekproject.eu',
-            'version'         => '1.0.0',
-        );
-    }
-
-    /**
      * @see Piwik\Plugin::getListHooksRegistered
      */
     public function getListHooksRegistered()
     {
         return array(
-            'AssetManager.getJsFiles'  => 'getJsFiles',
-            'AssetManager.getCssFiles'  => 'getCssFiles',
+            'AssetManager.getJavaScriptFiles'  => 'getJsFiles',
+            'AssetManager.getStylesheetFiles'  => 'getCssFiles',
         );
     }
 
